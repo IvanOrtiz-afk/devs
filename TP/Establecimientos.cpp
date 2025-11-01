64d3e001d30dbdd8b487cfe1690ab01c3f7340f6
IvanOrtiz <ivanlinking91@gmail.com>
2025-11-01 20:55:09 -0300

    Creacion de archivo establecimientos

--- TP/Establecimientos.cpp
+++ TP/Establecimientos.cpp
@@ -1,11 +1,57 @@
 #include <iostream>
+#include <cstring>
 #include "Establecimientos.h"
 
-using namespace std;
-
-Establecimientos::Establecimientos()
+Establecimientos::Establecimientos(int idestablecimiento, const char *nombreestablecimiento, const char *direccionesta, const char*tipoesta)
 {
-    //ctor
+    _idestablecimiento = idestablecimiento;
+    // 1. Copiar el nombre de forma segura strncopy(_direcciondestino, direccionorigen, tama¤o en bytes a copiar en el destino -1 (para poder poner luego el \0)
+    strncpy(_nombreestablecimiento, nombreestablecimiento, sizeof(_nombreestablecimiento) - 1);
+    // Asegurar que la cadena siempre termine con \0
+    _nombreestablecimiento[sizeof(_nombreestablecimiento) - 1] = '\0';
+
+    strncpy(_direccionesta, direccionesta, sizeof(_direccionesta) - 1);
+    _direccionesta[sizeof(_direccionesta) - 1] = '\0';
+
+    strncpy(_tipoesta, tipoesta, sizeof(_tipoesta) - 1);
+    _tipoesta[sizeof(_tipoesta) - 1] = '\0';
 }
+
+int Establecimientos::getidestablecimiento()
+{
+    return _idestablecimiento;
+};
+const char * Establecimientos::getnombreestablecimiento()
+{
+    return _nombreestablecimiento;
+};
+const char * Establecimientos::getdireccionesta()
+{
+    return _direccionesta;
+};
+const char * Establecimientos::gettipoesta()
+{
+    return _tipoesta;
+};
+void Establecimientos::setidestablecimiento(int idestablecimiento)
+{
+    _idestablecimiento=idestablecimiento;
+
+};
+void Establecimientos::setnombreestablecimiento(const char* nombreestablecimiento)
+{
+    strncpy(_nombreestablecimiento, nombreestablecimiento, sizeof(_nombreestablecimiento) - 1);
+    _nombreestablecimiento[sizeof(_nombreestablecimiento) - 1] = '\0';
+};
+void Establecimientos::setdireccionesta(const char* direccionesta)
+{
+    strncpy(_direccionesta, direccionesta, sizeof(_direccionesta) - 1);
+    _direccionesta[sizeof(_direccionesta) - 1] = '\0';
+};
+void Establecimientos::settipoesta(const char* tipoesta)
+{
+    strncpy(_tipoesta, tipoesta, sizeof(_tipoesta) - 1);
+    _tipoesta[sizeof(_tipoesta) - 1] = '\0';
+};
 
 
--- TP/Establecimientos.h
+++ TP/Establecimientos.h
@@ -4,9 +4,15 @@
 class Establecimientos
 {
     public:
-        Establecimientos();
+        Establecimientos(int idestablecimiento, const char *nombreestablecimiento, const char *direccionesta, const char*tipoesta);
         int getidestablecimiento();
-        char *getnombreestablecimiento();
+        const char *getnombreestablecimiento();
+        const char *getdireccionesta();
+        const char *gettipoesta();
+        void setidestablecimiento(int);
+        void setnombreestablecimiento(const char*);
+        void setdireccionesta(const char*);
+        void settipoesta(const char*);
 
     protected:
         int _idestablecimiento;
