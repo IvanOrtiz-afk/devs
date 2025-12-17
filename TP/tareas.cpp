25056d3a47eae5b8f8a37f51b6dfb1ed8670ab01
IvanOrtiz <ivanlinking91@gmail.com>
2025-12-16 22:11:09 -0300

    Agregado de txt de tareas pendientes

--- TP/tareas.cpp
+++ TP/tareas.cpp
@@ -1,44 +1,68 @@
 /**
+IVAN
+* Actualizar el informe!!!!
+
+IVAN
 * Tener la opci¢n de cancelar un consumo despu‚s de haber elegido un men£ para comer pero antes de pagar TODO EN EL MOMENTO. Que la funcion tipo_consumo pueda avisar al resto si el cliente desea cancelar toda la operaci¢n
 
+EVE
 * Quise ver menues filtrados por fecha, puse por error a¤o 1015 y el programa no lo noto (lo mismo con el dis y el mes)
 VALIDAR TODO
 
+EVE
 * Al cargar men£ de toda la semana si pulso cero sigue con la siguiente fecha en vez de volver al men£ principal. Esta opci¢n usarla por si el cliente quiere cargar menues saltando d¡as, pero agregar algo para que el cliente pueda cancelar todo el proceso y volver al men£ principal
 
+EVE
 * Al cargar un men£ nuevo puedo darle como nombre NUMEROS, esta mal
 
+EVE
 * Al darle valor a un plato, si le agrego un precio negativo termina la carga
 
+EVE
 * Al darle tipo de men£ (est ndar, caliaco, veg) si le doy letras termina la carga sin ninguna validaci¢n, posiblemente pase lo mismo con valores negativos o fuera del rango (solamente funciona si son letras)
 
+IVAN
 * Agregar opci¢n de listar menues de toda la semana
 
+IVAN
 * Valorar platos POR FECHA y no solo los del dia de hoy
 
+IVAN
 * Al seleccionar un ID para valorar un plato, no se distingue bien los ID, emprolijar eso
 
+IVAN
 * Al agregar una valoraci¢n ya VALIDA, no me muestra ning£n texto de confirmaci¢n
 
+IVAN
 * Listar valoraci¢n tiene que preguntar el establecimiento primero
 
+IVAN
 * El top 3 de valoraci¢n tiene tambi‚n que preguntar establecimiento antes y mostrar el nombre de cada men£ del top con SOLAMENTE los menus del establecimiento preguntado
 
+EVE
 * Revisar en todas las entradas validas que este el IF de si pulso 0 volver atr s
 
+IVAN/EVE AL FINAL
 * VER SI SALE, una funci¢n que muestre los platos mas consumidos pero de todos los tiempos (usar memoria din mica)
 
+IVAN
 * Arreglar el listado de platos mas vendidos BUG
 
+EVE
 * Ver todos los system pause y cls que se repiten mucho
 
+EVE
 * Las facturas manuales deber¡an cargar el importe autom ticamente basado en los menues existentes
 
+EVE
 * Cargar factura manual con algunos bugs
 
+EVE
 * Al cargar una factura manual se debe tener en cuenta el medio de pago
 
+IVAN
 * Clase cuenta corriente chequear y emprolijar dado que no se entiende, tambi‚n crear un tostring que sirva para visualizar movimientos
 
+EVE
 * Funcion para que un admin pueda ver listado todos los usuarios (nombres y rol solamente)
 */
